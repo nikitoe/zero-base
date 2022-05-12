148d88159306e777006fe5881e57a037a552470a
Jiyong Sim <sjy19910222@gmail.com>
2022-05-12 21:34:30 +0900

    변수와 자료형_3 추가

--- 코딩예제/JAVA/src/Main.java
+++ 코딩예제/JAVA/src/Main.java
@@ -1,63 +1,86 @@
-// Java 프로그래밍 - 변수와 자료형_2
+// Java 프로그래밍 - 변수와 자료형_3
+
+
+
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
 
-//      1. 자료형 - 숫자
-        System.out.println("== 숫자 ==");
-//      1-1. 정수
-        
-        int intNum = 10;
-        System.out.println("intNum = " + intNum);
-        System.out.println(Integer.MIN_VALUE);
-        System.out.println(Integer.MAX_VALUE);
+//      1. 자료형 - 문자열
+        System.out.println("== 문자열 ==");
+        String s1 = "Hello World";
+        System.out.println("s1 = " + s1);
+        String s2 = "12345";
+        System.out.println("s2 = " + s2);
 
-        int intNum2 = Integer.MAX_VALUE;
-        System.out.println("intNum2 = " + intNum2);
-        int intNUm3 = Integer.MIN_VALUE + 1;
-        System.out.println("intNUm3 = " + intNUm3);
+//      1-1. equals
+        String s3 = "Hi";
+        String s4 = "Hi";
+        System.out.println(s3.equals(s4));
+        System.out.println(s3 == s4);
 
-        long longNum = (long)Integer.MAX_VALUE + 1;
-        System.out.println("longNum = " + longNum);
-//      1-2. 실수
-        float floatNum = 1.23f;
-        double doubleNum = 1.23;
-        System.out.println(Float.MAX_VALUE);
-        System.out.println(Double.MAX_VALUE);
+        String s5 = new String("Hi");
+        System.out.println(s3.equals(s5));
+        System.out.println(s3 == s5);
 
-//      1-3. 2진수 / 8진수 / 16진수
-        int numBase2 = 0b1101110;
-        System.out.println("numBase2 = " + numBase2);
-        int numBase8 = 015;
-        System.out.println("numBase8 = " + numBase8);
-        int numBase16 = 0xC;
-        System.out.println("numBase16 = " + numBase16);
+//      1-2. indexOf
+        String s6 = "Hello! World!";
+        System.out.println(s6.indexOf("!"));
+        System.out.println(s6.indexOf("!",s6.indexOf("!") + 1));
 
-        System.out.println("0b" + Integer.toBinaryString(numBase2));
-        System.out.println("0" + Integer.toOctalString(numBase8));
-        System.out.println("0x" + Integer.toHexString(numBase16));
 
-//      2. 자료형 - 부울
-        System.out.println("== 부울 ==");
+//      1-3. replace
+        String s7 = s6.replace("Hello","Bye");
+        System.out.println("s7 = " + s7);
 
-        boolean isTrue = true;
-        boolean isFalse = false;
 
-        System.out.println(isTrue);
-        System.out.println(isFalse);
+//      1-4. substring
+        System.out.println(s7.substring(0, 3));
+        System.out.println(s7.substring(0, s7.indexOf("!") + 1));
 
-//      3. 자료형 - 문자
-        System.out.println("== 문자 ==");
+//      1-5. toUpperCase
+        System.out.println(s7.toUpperCase());
 
-        char keyFirst = 'a';
-        System.out.println("keyFirst = " + keyFirst);
-        char keyLast = 'z';
-        System.out.println("keyLast = " + keyLast);
 
-        System.out.println((int)keyFirst);
-        System.out.println((int)keyLast);
+//      2. 자료형 - StringBuffer
+        System.out.println("== StringBuffer ==");
+        StringBuffer sb1 = new StringBuffer();
+        sb1.append("01234");
+        System.out.println("sb1 = " + sb1);
+        sb1.append("5679");
+        System.out.println("sb1 = " + sb1);
 
-;
+        String a = "01234";
+        String b = "56789";
+        String bak = a;
+        System.out.println(a == bak);
+
+        a += b;
+        System.out.println(a);
+        System.out.println(a == bak);
+
+
+//      3. 자료형 - 배열
+        System.out.println("== 배열 ==");
+        int[] myArray = {1,2,3};
+        System.out.println(myArray[0]);
+        System.out.println(myArray[1]);
+        System.out.println(myArray[2]);
+
+        char[] myArray2 = {'a','b','c'};
+        System.out.println(myArray2[0]);
+        System.out.println(myArray2[1]);
+        System.out.println(myArray2[2]);
+
+        String[] myArray3 = new String[3];
+        myArray3[0] = "Hello";
+        myArray3[1] = " ";
+        myArray3[2] = "World";
+        System.out.println(myArray3[0]);
+        System.out.println(myArray3[1]);
+        System.out.println(myArray3[2]);
+
 
     }
 }
