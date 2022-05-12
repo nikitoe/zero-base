144478b3f555f25d771c2e1cd644e24abfd15852
Jiyong Sim <sjy19910222@gmail.com>
2022-05-12 22:14:21 +0900

    여러가지 연산자_1 추가

--- 코딩예제/JAVA/src/Main.java
+++ 코딩예제/JAVA/src/Main.java
@@ -1,118 +1,76 @@
-// Java 프로그래밍 - 변수와 자료형_4
-
-
-import java.sql.Array;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
+// Java 프로그래밍 - 여러가지 연산자_1
 
 public class Main {
     public static void main(String[] args) {
 
-//      1. 자료형 - 리스트
-        System.out.println("== 리스트 ==");
-        ArrayList l1 = new ArrayList();
+//      1. 대입 연산, 부호 연산자
+        int num = 100;
+        num = 10;
+        num = -10;
+        num = +10;
 
-//      1-1. add
-        l1.add(1);
-        l1.add("Hello");
-        l1.add(2);
-        l1.add(3);
-        l1.add(4);
-        l1.add("World!");
-        System.out.println("l1 = " + l1);
-
-        l1.add(0,10);
-        System.out.println("l1 = " + l1);
-
-//      1-2. get
-        System.out.println(l1.get(0));
-        System.out.println(l1.get(3));
-
-//      1-3. size
-        System.out.println(l1.size());
-
-//      1-4. remove
-
-        System.out.println(l1.remove(0));
-        System.out.println("l1 = " + l1);
-        System.out.println(l1.remove(Integer.valueOf(2)));
-        System.out.println("l1 = " + l1);
+//      2. 산술 연산자, 증가/감소 연산자
+        System.out.println("== 산술 연산자, 증가/감소 연산자 ==");
+        int numX = 10;
+        int numY = 3;
+        int result = 0;
+        result = numX + numY;
+        System.out.println(result);
+        result = numX - numY;
+        System.out.println(result);
+        result = numX * numY;
+        System.out.println(result);
+        result = numX % numY;
 
 
+        int numZ = 1;
+        System.out.println(numZ++);
+        System.out.println(numZ);
 
-//      1-5. clear
-        l1.clear();
-        System.out.println("l1 = " + l1);
+        numZ = 1;
+        System.out.println(++numZ);
+        System.out.println(numZ);
 
-//      1-6. sort
-        ArrayList l2 = new ArrayList();
-        l2.add(5);
-        l2.add(6);
-        l2.add(7);
-        System.out.println("l2 = " + l2);
-        
-        l2.sort(Comparator.naturalOrder()); // 오름차순
-        System.out.println("l2 = " + l2);
+        numZ = 1;
+        System.out.println(numZ--);
+        System.out.println(numZ);
 
-        l2.sort(Comparator.reverseOrder()); // 내림차순
-        System.out.println("l2 = " + l2);
+        numZ = 1;
+        System.out.println(--numZ);
+        System.out.println(numZ);
 
 
+//      3. 관계 연산자
+        System.out.println("== 관계 연산자 ==");
 
-//      1-7. contains
-        System.out.println(l2.contains(1));
-        System.out.println(l2.contains(5));
+        int numA = 10;
+        int numB = 9;
 
-
-//      2. Maps
-        System.out.println("== Maps ==");
-        HashMap map = new HashMap();
-
-//      2-1. put
-        map.put("Kiwi", 9000);
-        map.put("banana", 1000);
-        map.put("apple", 800);
-        System.out.println("map = " + map);
-
-//      2-2. get
-        System.out.println(map.get("mandarin"));
-        System.out.println(map.get("apple"));
+        System.out.println(numA > numB);
+        System.out.println(numA < numB);
+        System.out.println(numA == numB);
+        System.out.println(numA != numB);
 
 
-//      2-3. size
-        System.out.println(map.size());
-
-//      2-4. remove
-        System.out.println(map.remove("Kiwi"));
-        System.out.println(map.remove("Mandarin"));
-        System.out.println("map = " + map);
-
-//      2-5. containsKey
-        System.out.println(map.containsKey("apple"));
-        System.out.println(map.containsKey("Kiwi"));
+//      4. 논리 연산자
+        System.out.println("== 논리 연산자 ==");
+        System.out.println((10 > 9) && (1 ==0));
+        System.out.println((10 > 9) || (1 ==0));
 
 
-//      3. Generics
-        System.out.println("== Generics ==");
-        ArrayList l3 = new ArrayList();
-        l3.add(1);
-        l3.add("Hello");
-        System.out.println("l3 = " + l3);
+//      5. 복합 대입 연산자
+        System.out.println("== 복합 대입 연산자 ==");
+        int num1 = 10;
+        int num2 = 5;
+        num1 += num2;
+        System.out.println("num2 = " + num2);
+        System.out.println(num1);
 
-        ArrayList<String> l4 = new ArrayList<String>();
-       // l4.add(1);
-        l4.add("Hello");
-        System.out.println("l4 = " + l4);
-
-        HashMap map2 = new HashMap();
-        map2.put(123,"ID");
-        System.out.println("map2 = " + map2);
-
-        HashMap<String,Integer> map3 = new HashMap<String, Integer>();
-        //map3.put(123,123);
-        map3.put("ID",123);
-        System.out.println("map3 = " + map3);
+        //      6. 삼항 연산자
+        System.out.println("== 삼항 연산자 ==");
+        int a = 100;
+        String aResult = (a == 100) ? "yes" :"no";
+        System.out.println("aResult = " + aResult);
 
     }
 
