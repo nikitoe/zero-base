e04de0c23c51dad85e9c26cadfd997ae5b6871ee
Jiyong Sim <sjy19910222@gmail.com>
2022-05-13 16:33:41 +0900

    여러가지 연산자_2 추가

--- 코딩예제/JAVA/src/Main.java
+++ 코딩예제/JAVA/src/Main.java
@@ -1,76 +1,58 @@
-// Java 프로그래밍 - 여러가지 연산자_1
+// Java 프로그래밍 - 여러가지 연산자_2
 
 public class Main {
     public static void main(String[] args) {
 
-//      1. 대입 연산, 부호 연산자
-        int num = 100;
-        num = 10;
-        num = -10;
-        num = +10;
-
-//      2. 산술 연산자, 증가/감소 연산자
-        System.out.println("== 산술 연산자, 증가/감소 연산자 ==");
-        int numX = 10;
-        int numY = 3;
+//      1. 비트 논리 연산자
+        System.out.println("== 비트 논리 연산자 ==");
+//      1-1. AND 연산자 (&)
+        int num1 = 5;
+        int num2 = 3;
         int result = 0;
-        result = numX + numY;
-        System.out.println(result);
-        result = numX - numY;
-        System.out.println(result);
-        result = numX * numY;
-        System.out.println(result);
-        result = numX % numY;
+
+        result = num1 & num2;
+        System.out.println("result = " + result);
+        System.out.println(Integer.toBinaryString(num1));
+        System.out.println(Integer.toBinaryString(num2));
+
+        System.out.printf("%04d\n", Integer.parseInt(Integer.toBinaryString(num1)));
+        System.out.printf("%04d\n", Integer.parseInt(Integer.toBinaryString(num2)));
+        System.out.printf("%04d\n", Integer.parseInt(Integer.toBinaryString(result)));
+
+//      1-2. OR 연산자 (|)
+
+        result = num1 | num2;
+        System.out.println("result = " + result);
+        System.out.printf("%04d\n", Integer.parseInt(Integer.toBinaryString(num1)));
+        System.out.printf("%04d\n", Integer.parseInt(Integer.toBinaryString(num2)));
+        System.out.printf("%04d\n", Integer.parseInt(Integer.toBinaryString(result)));
 
 
-        int numZ = 1;
-        System.out.println(numZ++);
-        System.out.println(numZ);
 
-        numZ = 1;
-        System.out.println(++numZ);
-        System.out.println(numZ);
+//      1-3. XOR 연산자 (^)
 
-        numZ = 1;
-        System.out.println(numZ--);
-        System.out.println(numZ);
+        result = num1 ^ num2;
+        System.out.println("result = " + result);
+        System.out.printf("%04d\n", Integer.parseInt(Integer.toBinaryString(num1)));
+        System.out.printf("%04d\n", Integer.parseInt(Integer.toBinaryString(num2)));
+        System.out.printf("%04d\n", Integer.parseInt(Integer.toBinaryString(result)));
 
-        numZ = 1;
-        System.out.println(--numZ);
-        System.out.println(numZ);
+//      1-4. 반전 연산자 (~)
+
+        result = num1 ~ num2;
 
 
-//      3. 관계 연산자
-        System.out.println("== 관계 연산자 ==");
 
-        int numA = 10;
-        int numB = 9;
-
-        System.out.println(numA > numB);
-        System.out.println(numA < numB);
-        System.out.println(numA == numB);
-        System.out.println(numA != numB);
+//      2. 비트 이동 연산자
+        System.out.println("== 비트 이동 연산자 ==");
+//      2-1. << 연산자
 
 
-//      4. 논리 연산자
-        System.out.println("== 논리 연산자 ==");
-        System.out.println((10 > 9) && (1 ==0));
-        System.out.println((10 > 9) || (1 ==0));
+//      2-2. >> 연산자
 
 
-//      5. 복합 대입 연산자
-        System.out.println("== 복합 대입 연산자 ==");
-        int num1 = 10;
-        int num2 = 5;
-        num1 += num2;
-        System.out.println("num2 = " + num2);
-        System.out.println(num1);
+//      2-3. >>> 연산자
 
-        //      6. 삼항 연산자
-        System.out.println("== 삼항 연산자 ==");
-        int a = 100;
-        String aResult = (a == 100) ? "yes" :"no";
-        System.out.println("aResult = " + aResult);
 
     }
 
