d747feb1d016a07451c87fabdf50d8ccec9a73a6
Jiyong Sim <sjy19910222@gmail.com>
2022-05-14 17:06:31 +0900

    여러가지 연산자_2 수정

--- 코딩예제/JAVA/src/Main.java
+++ 코딩예제/JAVA/src/Main.java
@@ -20,6 +20,7 @@ public class Main {
         System.out.printf("%04d\n", Integer.parseInt(Integer.toBinaryString(result)));
 
 //      1-2. OR 연산자 (|)
+        System.out.println("== | ==");
 
         result = num1 | num2;
         System.out.println("result = " + result);
@@ -30,7 +31,7 @@ public class Main {
 
 
 //      1-3. XOR 연산자 (^)
-
+        System.out.println("== XOR ==");
         result = num1 ^ num2;
         System.out.println("result = " + result);
         System.out.printf("%04d\n", Integer.parseInt(Integer.toBinaryString(num1)));
@@ -38,20 +39,44 @@ public class Main {
         System.out.printf("%04d\n", Integer.parseInt(Integer.toBinaryString(result)));
 
 //      1-4. 반전 연산자 (~)
+        System.out.println("== ~ ==");
+        num1 =5;
 
-        result = num1 ~ num2;
+        result = ~num1;
+        System.out.println("result = " + result);
+        System.out.printf("%04d\n", Integer.parseInt(Integer.toBinaryString(num1)));
+        System.out.printf("%s\n", Integer.toBinaryString(result));
 
 
 
 //      2. 비트 이동 연산자
         System.out.println("== 비트 이동 연산자 ==");
 //      2-1. << 연산자
+        int numA = 3;
+        result = numA << 1;
 
+        System.out.println("numA = " + numA);
+        System.out.println("result = " + result);
+        System.out.printf("%04d\n", Integer.parseInt(Integer.toBinaryString(numA)));
+        System.out.printf("%04d\n", Integer.parseInt(Integer.toBinaryString(result )));
 
 //      2-2. >> 연산자
 
-
+        result = numA >> 1;
+        System.out.println("numA = " + numA);
+        System.out.println("result = " + result);
+        System.out.printf("%04d\n", Integer.parseInt(Integer.toBinaryString(numA)));
+        System.out.printf("%04d\n", Integer.parseInt(Integer.toBinaryString(result )));
 //      2-3. >>> 연산자
+
+        numA = -5;
+        result = numA >>1;
+        System.out.printf("%s\n", Integer.toBinaryString(numA));
+        System.out.printf("%s\n", Integer.toBinaryString(result));
+
+        result = numA >>> 1;
+        System.out.printf("%s\n", Integer.toBinaryString(numA));
+        System.out.printf("%s\n", Integer.toBinaryString(result));
 
 
     }
