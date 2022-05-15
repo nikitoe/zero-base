c34ec0ed7d0c2908484c0e218c1c2bf965983440
Jiyong Sim <sjy19910222@gmail.com>
2022-05-15 18:27:08 +0900

    인터페이스 추가

--- 코딩예제/JAVA/src/Practice.java
+++ 코딩예제/JAVA/src/Practice.java
@@ -1,84 +1,95 @@
 // Practice
-// 아래 Device 추상 클래스를 이용하여
-// UsbPort1 클래스와 WiFi 클래스를 자유롭게 구현해보세요.
+// 아래 GreenOrc 클래스와 NPCSystem, UserSystem 인터페이스를 이용하여
+// OrkNPC1, OrkUser1 클래스를 만들어보세요.
 
-abstract class Device {
-    int deviceId;
 
-    abstract void deviceInfo();
-    abstract void connect();
-    abstract void disconnect();
-    abstract void send();
-    abstract void receive();
+abstract class GreenOrc {
+    public final String SKIN_COLOR = "녹색";
+    public int health;
+    public int attackDamage;
+    public int defense;
+
+    public abstract void setHealth();
+    public abstract void setDamage();
+    public abstract void setDefense();
 }
 
-// UsbPort1 클래스
-class UsbPort1 extends Device{
-    UsbPort1(int id){
-        this.deviceId = id;
+interface NPCSystem {
+    public abstract void conversationSystem();
+    public abstract void questionSystem();
+}
+
+interface UserSystem {
+    public abstract void partySystem();
+    public abstract void tradeSystem();
+}
+
+// OrkNPC1 클래스
+class OrkNPC1 extends GreenOrc implements NPCSystem {
+
+    @Override
+    public void setHealth() {
+        this.health = 100;
     }
 
-    void deviceInfo(){
-        System.out.println("id = " + deviceId);
+    @Override
+    public void setDamage() {
+        this.attackDamage = 10;
     }
 
-    void connect(){
-        System.out.println("연결 하였습니다.");
+    @Override
+    public void setDefense() {
+        this.defense = 5;
     }
 
-    void disconnect(){
-        System.out.println("연결이 해지되었습니다..");
+    @Override
+    public void conversationSystem() {
+        System.out.println("안녕");
+        System.out.println("요즘 새로운 소식 없나요?");
     }
 
-    void send(){
-        System.out.println("데이터를 전송합니다.");
-    }
-
-    void receive(){
-        System.out.println("연결 하였습니다.");
+    @Override
+    public void questionSystem() {
+        System.out.println("새로운 퀘스트");
+        System.out.println("퀘스트 완료");
     }
 }
 
-// WiFi 클래스
-class WiFi extends Device{
+// OrkUser1 클래스
+class OrkUser1 extends GreenOrc implements UserSystem{
 
-    public WiFi(int id){
-        this.deviceId = id;
+    @Override
+    public void setHealth() {
+        this.health = 200;
     }
 
     @Override
-    void deviceInfo() {
-
+    public void setDamage() {
+        this.attackDamage = 20;
     }
 
     @Override
-    void connect() {
-
+    public void setDefense() {
+        this.defense = 10;
     }
 
     @Override
-    void disconnect() {
-
+    public void partySystem() {
+        System.out.println("파티 초대");
+        System.out.println("파티 수락");
     }
 
     @Override
-    void send() {
-
-    }
-
-    @Override
-    void receive() {
-
+    public void tradeSystem() {
+        System.out.println("거래 신청");
+        System.out.println("거래 완료");
     }
 }
+
 
 public class Practice {
-
     public static void main(String[] args) {
         // Test code
-        UsbPort1 usb1 = new UsbPort1(1);
-        usb1.connect();
-        WiFi wifi = new WiFi(0);
+        // No test code
     }
-
 }
