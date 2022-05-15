678773f02c51c9b732c3f23596a5aeb6508d2f74
Jiyong Sim <sjy19910222@gmail.com>
2022-05-15 17:33:43 +0900

    추상클래스 추가

--- 코딩예제/JAVA/src/Main.java
+++ 코딩예제/JAVA/src/Main.java
@@ -1,100 +1,33 @@
-// Java 프로그래밍 - 다형성
+// Java 프로그래밍 - 추상 클래스
 
-class Person {
-    public void print() {
-        System.out.println("Person.print");
-    }
+// 추상 클래스 Person
+abstract class Person{
+    abstract void printInfo();
 }
 
-class Student extends Person {
-    public void print() {
-        System.out.println("Student.print");
-    }
 
-    public void print2() {
-        System.out.println("Student.print2");
+// 추상 클래스 상속
+class Student extends Person{
+    public void printInfo(){
+        System.out.println("Student.printInfo");
     }
 }
-
-class CollegeStudent extends Person {
-    public void print() {
-        System.out.println("CollegeStudent.print");
-    }
-}
-
 
 public class Main {
 
     public static void main(String[] args) {
-
-//      1. 다형성
-        System.out.println("== 다형성 ==");
-        Person p1 = new Person();
-        Student s1 = new Student();
-
-        Person p2 = new Student();
-
-        p1.print();
-        s1.print();
-        s1.print2();
-        p2.print();
-//        p2.print2();
-
-
-        Person p3 = new CollegeStudent();
-        p3.print();
-
-
-
-//      2. 타입 변환
-        System.out.println("== 타입 변환 ==");
-        Person pp1 = null;
-        Student ss1 = null;
-
-        Person pp2 = new Person();
-        Student ss2 = new Student();
-        Person pp3 = new Student(); //  업태스팅
-
-        pp1 = pp2;
-        pp1 = ss2;
         
-        ss1 = ss2;
-//        ss1 = pp2;
-        ss1 = (Student) pp3;    // 다운 캐스팅
+//        추상 클래스의 사용
+        Student s1 = new Student();
+        s1.printInfo();
 
-        CollegeStudent cc1;
-        CollegeStudent cc2 = new CollegeStudent();
-//        ss1 = (Stduent)cc2;
-
-
-//      3. instanceof
-        System.out.println("== instanceof ==");
-        Person pe1 = new Person();
-        Student st1 = new Student();
-        Person pe2 = new Student();
-        Person pe3 = new CollegeStudent();
-
-        System.out.println(pe1 instanceof  Person);
-        System.out.println(pe1 instanceof  Student);
-
-        System.out.println(st1 instanceof  Person);
-        System.out.println(st1 instanceof  Student);
-
-        System.out.println(pe2 instanceof  Person);
-        System.out.println(pe2 instanceof  Student);
-
-        System.out.println(pe3 instanceof  Person);
-        System.out.println(pe3 instanceof  CollegeStudent);
-
-        if(pe1 instanceof  Student){
-            Student stu1 = (Student) pe1;
-        }
-
-        if(st1 instanceof  Person){
-            Person per1 = (Person) st1;
-        }
-
-
-
+        Person p2 = new Person() {
+            @Override
+            void printInfo() {
+                System.out.println("Main.printInfo");
+            }
+        };
+        p2.printInfo();
     }
+
 }
--- 코딩예제/JAVA/src/Practice.java
+++ 코딩예제/JAVA/src/Practice.java
@@ -1,46 +1,84 @@
 // Practice
-// 아래의 클래스와 상속 관계에서 다형성을 이용하여
-// Car 객체를 통해 아래와 같이 출력될 수 있도록 Test code 부분을 구현해보세요.
-// 빵빵!
-// 위이잉!
-// 삐뽀삐뽀!
+// 아래 Device 추상 클래스를 이용하여
+// UsbPort1 클래스와 WiFi 클래스를 자유롭게 구현해보세요.
 
-class Car {
-    Car(){}
-    public void horn() {
-        System.out.println("빵빵!");
+abstract class Device {
+    int deviceId;
+
+    abstract void deviceInfo();
+    abstract void connect();
+    abstract void disconnect();
+    abstract void send();
+    abstract void receive();
+}
+
+// UsbPort1 클래스
+class UsbPort1 extends Device{
+    UsbPort1(int id){
+        this.deviceId = id;
+    }
+
+    void deviceInfo(){
+        System.out.println("id = " + deviceId);
+    }
+
+    void connect(){
+        System.out.println("연결 하였습니다.");
+    }
+
+    void disconnect(){
+        System.out.println("연결이 해지되었습니다..");
+    }
+
+    void send(){
+        System.out.println("데이터를 전송합니다.");
+    }
+
+    void receive(){
+        System.out.println("연결 하였습니다.");
     }
 }
 
-class FireTruck extends Car {
-    public void horn() {
-        System.out.println("위이잉!");
-    }
-}
+// WiFi 클래스
+class WiFi extends Device{
 
-class Ambulance extends Car {
-    public void horn() {
-        System.out.println("삐뽀삐뽀!");
+    public WiFi(int id){
+        this.deviceId = id;
+    }
+
+    @Override
+    void deviceInfo() {
+
+    }
+
+    @Override
+    void connect() {
+
+    }
+
+    @Override
+    void disconnect() {
+
+    }
+
+    @Override
+    void send() {
+
+    }
+
+    @Override
+    void receive() {
+
     }
 }
 
 public class Practice {
+
     public static void main(String[] args) {
         // Test code
-
-        Car car = new Car();
-        car.horn();
-
-        car = new FireTruck();
-        car.horn();
-
-        car= new Ambulance();
-        car.horn();
-
-        Car car2[] = {new Car(), new FireTruck(), new Ambulance()};
-
-        for(Car item: car2){
-            item.horn();
-        }
+        UsbPort1 usb1 = new UsbPort1(1);
+        usb1.connect();
+        WiFi wifi = new WiFi(0);
     }
+
 }
