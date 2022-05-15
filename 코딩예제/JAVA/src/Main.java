ebe6a4537950bfe06162be7745217f4e5ad5fffc
Jiyong Sim <sjy19910222@gmail.com>
2022-05-16 02:14:13 +0900

    입출력_1 추가

--- 코딩예제/JAVA/src/Main.java
+++ 코딩예제/JAVA/src/Main.java
@@ -1,54 +1,86 @@
-// Java 프로그래밍 - 내부 클래스
+// Java 프로그래밍 - 입출력_1
 
-// 내부 클래스 구조
-class Outer{
-    public void print(){
-        System.out.println("Outer.print");
-    }
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
-
-    class Inner{
-        public void innerPrint(){
-            Outer.this.print();
-        }
-    }
-
-    static class InnerStaticClass{
-        void innerPrint(){
-//            Outer.this.print();
-        }
-    }
-}
-abstract class Person{
-    public abstract void printInfo();
-}
-
-class Student extends Person{
-
-    @Override
-    public void printInfo() {
-        System.out.println("Student.printInfo");
-    }
-}
 public class Main {
 
-    public static void main(String[] args) {
+    public static void referInputStream() throws IOException {
+//      System.in
+//        System.out.println("== System.in ==");
+//        System.out.println("입력 : ");
+//        int a = System.in.read() - '0';
+//        System.out.println("a = " + a);
+//        System.in.read(new byte[System.in.available()]);
 
-//      외부 클래스
-        Outer o1 = new Outer();
+//      InputStreamReader
+//        System.out.println("== InputStreamReader ==");
+//        InputStreamReader reader = new InputStreamReader(System.in);
+//        char[] c = new char[3];
+//        System.out.println("입력 : ");
+//        reader.read(c);
+//        System.out.println(c);
 
-//      내부 클래스 - 인스턴스
-        Outer.Inner i1 = new Outer().new Inner();
-//      내부 클래스 - 정적
-        Outer.InnerStaticClass is1 = new Outer.InnerStaticClass();
-//      익명 클래스
-        Person p1 = new Person(){
 
-            @Override
-            public void printInfo() {
-                System.out.println("Main.printInfo");
-            }
-        };
+//      BufferedReader
+//        System.out.println("== BufferedReader ==");
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        System.out.println("입력 : ");
+//        String s1 = br.readLine();
+//        System.out.println("s1 = " + s1);
     }
 
+    public static void main(String[] args) throws IOException {
+
+//      1. 입력
+//      1-1. 다른 입력 방식 참고
+//        referInputStream();
+
+//      1-2. Scanner
+        System.out.println("== Scanner ==");
+        Scanner sc = new Scanner(System.in);
+//        System.out.println("입력1: ");
+//        System.out.println(sc.next());
+//        sc.nextLine();
+
+//        System.out.println("입력2: ");
+//        System.out.println(sc.nextInt());
+//        sc.nextLine();
+
+//        System.out.println("입력3: ");
+//        System.out.println(sc.nextLine());
+
+//      참고) 정수, 문자열 변환
+        int num = Integer.parseInt("12345");
+        String str = Integer.toString(12345);
+        
+//      2. 출력
+        System.out.println("== 출력 ==");
+
+        String s = "자바";
+        int number = 3;
+
+        System.out.println(s + "는 언어 선호도" +number+"위 입니다.");
+        System.out.printf("%s는 언어 선호도 %d위 입니다.\n",s,number);
+
+        System.out.printf("%d\n",10);
+        System.out.printf("%o\n",10);
+        System.out.printf("%x\n",10);
+        System.out.printf("%f\n",5.2f);
+        System.out.printf("%c\n",'A');
+        System.out.printf("%s\n","String");
+
+        System.out.printf("%5d\n",123);
+        System.out.printf("%-5d\n",123);
+        System.out.printf("%5d\n",12345);
+        System.out.printf("%5d\n",12345);
+
+        System.out.printf("%.2f\n",1.23123132);
+
+
+
+    }
 }
