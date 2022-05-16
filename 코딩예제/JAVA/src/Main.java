bca51443dd9e495cbecb549b8d58406a7c3cc2f0
Jiyong Sim <sjy19910222@gmail.com>
2022-05-16 18:51:49 +0900

    컬렉션 프레임워크 추가

--- 코딩예제/JAVA/src/Main.java
+++ 코딩예제/JAVA/src/Main.java
@@ -1,97 +1,110 @@
-// Java 프로그래밍 - 예외 처리
+// Java 프로그래밍 - 컬렉션 프레임워크
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-
-
-class NotTenException extends RuntimeException{
-
-}
+import java.util.*;
 
 public class Main {
 
-    public static boolean checkTen(int ten) {
-        if (ten != 10) {
-            return false;
-        }
+    public static void main(String[] args) {
 
-        return true;
-    }
+//      1. List
+//        1-1. ArrayList
+        ArrayList list1 = new ArrayList();
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        System.out.println("list1 = " + list1);
+        list1.remove(Integer.valueOf(2));
+        System.out.println("list1 = " + list1);
+        list1.add(0, 10);
+        System.out.println("list1 = " + list1);
+        System.out.println("list1.size() = " + list1.size());
+        System.out.println("list1.contains(1) = " + list1.contains(1));
+        System.out.println("list1.indexOf(10) = " + list1.indexOf(10));
 
-    public static boolean checkTenWithException(int ten) {
-        try{
+//      1-2. LinkedList
+        System.out.println("== LinkedList ==");
+        LinkedList list2 = new LinkedList();
+        list2.add(1);
+        list2.add(2);
+        list2.add(3);
+        list2.add(4);
+        System.out.println("list2 = " + list2);
+        list2.addFirst(10);
+        list2.addLast(20);
+        System.out.println("list2 = " + list2);
+        list2.remove(Integer.valueOf(1));
+        System.out.println("list2 = " + list2);
+        list2.removeFirst();
+        list2.removeLast();
+        System.out.println("list2 = " + list2);
+        System.out.println(list2.size());
 
-            if(ten != 10){
-                throw new NotTenException();
-            }
+//      2. Set
+//      2-1. HashSet
+        System.out.println("== HashSet ==");
+        HashSet set1 = new HashSet();
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+        System.out.println("set1 = " + set1);
+        set1.remove(1);
+        System.out.println("set1 = " + set1);
+        set1.add(2);
+        set1.add(3);
+        System.out.println("set1 = " + set1);
 
-        } catch (NotTenException e) {
-            System.out.println("e = " + e);
-            return  false;
-        }
-        return true;
-    }
+//      2-2. TreeSet
+        System.out.println("== TreeSet ==");
+        TreeSet set2 = new TreeSet();
+        set2.add(1);
+        set2.add(2);
+        set2.add(3);
+        System.out.println("set2 = " + set2);
+        set2.remove(1);
+        System.out.println("set2 = " + set2);
+        set2.clear();
+        System.out.println("set2 = " + set2);
+        set2.add(10);
+        set2.add(5);
+        set2.add(3);
+        set2.add(3);
+        System.out.println("set2 = " + set2);
+        System.out.println(set2.first());
+        System.out.println(set2.last());
+        System.out.println(set2.lower(10));
+        System.out.println(set2.higher(1));
 
-    public static boolean checkTenWithThrows(int ten) throws  NotTenException{
-        if(ten != 10){
-            throw  new NotTenException();
-        }
 
-        return true;
-    }
-
-    public static void main(String[] args) throws IOException {
-
-//      1. 예외
-//      1-1. 0으로 나누기
-        System.out.println("== 0으로 나누기 ==");
-//        int a = 5 / 0;
-
-        try{
-            int a = 5 / 0;
-        }catch(ArithmeticException e) {
-            System.out.println("0으로 나누기 예외 발생");
-            System.out.println("e = " + e);
-        }finally {
-            System.out.println("1-1연습 종료");
-        }
-
-//      1-2. 배열 인덱스 초과
-        System.out.println("== 배열 인덱스 초과 ==");
-        int[] b = new int[4];
-//        b[4] = 1;
-        try{
-            b[4] = 1;
-        }catch(ArrayIndexOutOfBoundsException e) {
-            System.out.println("e = " + e);
-        }
+//      3. Map
+//      3-1. HashMap
+        System.out.println("== HashMap ==");
+        HashMap map1 = new HashMap();
+        map1.put(1, "kiwi");
+        map1.put(2, "apple");
+        map1.put(3, "mango");
+        System.out.println("map1 = " + map1);
         
-//      1-3. 없는 파일 열기
-        System.out.println("== 없는 파일 열기 ==");
-//        BufferedReader br = new BufferedReader(new FileReader("abc.txt"));
-
-//      2. throw, throws
-        System.out.println("== checkTen ==");
-        boolean checkResult = Main.checkTen(10);
-        System.out.println("checkResult = " + checkResult);
+        map1.remove(2);
+        System.out.println("map1 = " + map1);
+        System.out.println("map1.get(1) = " + map1.get(1));
+        System.out.println(set1.size());
+        System.out.println(set1.contains(2));
 
 
-        System.out.println("== checkTenWithException ==");
-        checkResult = checkResult = Main.checkTenWithException(11);
-        System.out.println("checkResult = " + checkResult);
-        
-        
-        System.out.println("== checkTenWithThrows ==");
-        try {
+//      3-2. TreeMap
+        System.out.println("== TreeMap ==");
+        TreeMap map2 = new TreeMap();
+        map2.put(10,"kiwi");
+        map2.put(5,"mango");
+        map2.put(15,"apple");
+        System.out.println("map2 = " + map2);
 
-            checkResult = checkTenWithException(5);
-
-        }catch(NotTenException e) {
-            System.out.println("e = " + e);
-        }
-        System.out.println("checkResult = " + checkResult);
+        System.out.println(map2.firstEntry());
+        System.out.println(map2.firstKey());
+        System.out.println(map2.lastEntry());
+        System.out.println(map2.lastKey());
+        System.out.println(map2.lowerEntry(10));
+        System.out.println(map2.higherEntry(10));
 
     }
-
 }
