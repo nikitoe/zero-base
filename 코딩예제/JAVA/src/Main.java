714cf630bcd4ef4f8ca8b014c878f449b071f764
Jiyong Sim <sjy19910222@gmail.com>
2022-05-24 21:17:44 +0900

    Part02 자료구조/알고리즘 03.순열 추가

--- 코딩예제/JAVA/src/Main.java
+++ 코딩예제/JAVA/src/Main.java
@@ -1,74 +1,62 @@
-// 기초 수학 - 경우의 수
+// 기초 수학 - 순열
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
 
-//      1. 합의 법칙
-        System.out.println("== 합의 법칙 ==");
-//      두 개의 주사를 던졌을 때 합이 3 또는 4의 배수일 경우의 수
+//      1. 팩토리얼
+        System.out.println("== 팩토리얼 ==");
+//      5!
+        int n = 5;
+        int result = 1;
 
-        int[] dice1 = {1, 2, 3, 4, 5, 6};
-        int[] dice2 = {1, 2, 3, 4, 5, 6};
-
-        int nA = 0;
-        int nB = 0;
-        int nAandB = 0;
-        
-        // 기본 풀이
-        for(int item1: dice1){
-            for(int item2 : dice2){
-                if((item1 + item2) % 3 == 0){
-                    nA += 1;
-                }
-
-                if((item1 + item2) % 4 == 0){
-                    nB += 1;
-                }
-
-                if((item1 + item2) % 12 == 0){
-                    nAandB += 1;
-                }
-            }
+        for (int i = n; i <= n ; i++) {
+            result *= i;
         }
+        System.out.println("result = " + result);
+        System.out.println(IntStream.range(2,6).reduce(1,(x ,y)->(x*y)));
 
-        System.out.println("결과" + (nA + nB - nAandB));
 
-        // HashSet 이용
-        HashSet<ArrayList> allCase = new HashSet<>();
-        for(int item1: dice1){
-            for(int item2: dice2){
-                if((item1 + item2) % 3 == 0 || (item1 + item2) % 4 == 0){
-                    ArrayList list = new ArrayList(Arrays.asList(item1, item2));
-                    allCase.add(list);
-                }
-            }
+
+//      2. 순열
+        System.out.println("== 순열 ==");
+//      5명을 3줄로 세우는 경우의 수
+        n = 5;
+        int r = 3;
+
+        for (int i = n; i >= n -r + 1; i--) {
+            result *= i;
         }
-        System.out.println("결과 : " + allCase.size());
+        System.out.println("result = " + result);
 
 
-//      2. 곱의 법칙
-        System.out.println("== 곱의 법칙 ==");
-//      두 개의 주사위 a, b를 던졌을 때 a는 3의 배수, b는 4의 배수인 경우의 수
-        nA = 0;
-        nB = 0;
+//      3. 중복 순열
+        System.out.println("== 중복 순열 ==");
+//      서로 다른 4개의 수 중 2개를 뽑는 경우의 수 (중복 허용)
+        n = 4;
+        r = 2;
+        result = 1;
 
-        for(int item1 : dice1){
-            if(item1 % 3 == 0){
-                nA++;
-            }
+
+        for (int i = 0; i < r; i++) {
+            result *= n;
         }
+        System.out.println(result);
 
-        for(int item2 : dice2){
-            if(item2 % 4 == 0){
-                nB++;
-            }
+        System.out.println(Math.pow(n,r));
+
+
+
+//      4. 원 순열
+        System.out.println("== 원 순열 ==");
+//      원 모양의 테이블에 3명을 앉히는 경우의 수
+
+        n = 3;
+        result = 1;
+        for (int i = 1; i < n; i++) {
+            result *= i;
+            System.out.println("result = " + result);
         }
-
-        System.out.println("결과 : " + (nA * nB));
-
     }
 }
