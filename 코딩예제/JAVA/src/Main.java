4cb0c815353671a50fe08f9bb06df85160f9721a
Jiyong Sim <sjy19910222@gmail.com>
2022-05-24 16:00:52 +0900

    Part02 자료구조/알고리즘 03.경우의 수 추가

--- 코딩예제/JAVA/src/Main.java
+++ 코딩예제/JAVA/src/Main.java
@@ -1,4 +1,4 @@
-// 기초 수학 - 집합
+// 기초 수학 - 경우의 수
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -7,37 +7,68 @@ import java.util.HashSet;
 public class Main {
     public static void main(String[] args) {
 
-//      1. 자바에서 집합 사용 - HashSet
-        System.out.println("== HashSet ==");
-        HashSet set1 = new HashSet();
-        set1.add(1);
-        set1.add(1);
-        set1.add(1);
-        System.out.println("set1 = " + set1);
-        set1.add(2);
-        set1.add(3);
-        System.out.println("set1 = " + set1);
-        set1.remove(1);
-        System.out.println("set1 = " + set1);
-        System.out.println(set1.size());
-        System.out.println(set1.contains(2));
+//      1. 합의 법칙
+        System.out.println("== 합의 법칙 ==");
+//      두 개의 주사를 던졌을 때 합이 3 또는 4의 배수일 경우의 수
+
+        int[] dice1 = {1, 2, 3, 4, 5, 6};
+        int[] dice2 = {1, 2, 3, 4, 5, 6};
+
+        int nA = 0;
+        int nB = 0;
+        int nAandB = 0;
         
+        // 기본 풀이
+        for(int item1: dice1){
+            for(int item2 : dice2){
+                if((item1 + item2) % 3 == 0){
+                    nA += 1;
+                }
 
-//      2. 집합 연산
-        System.out.println("== 집합 연산 ==");
+                if((item1 + item2) % 4 == 0){
+                    nB += 1;
+                }
 
-//      2-1. 교집합
-        HashSet a = new HashSet(Arrays.asList(1,2,3,4,5));
-        HashSet b = new HashSet(Arrays.asList(2,4,6,8,10));
-//        a.retainAll(b);
-//        System.out.println(" 교집합 = " + a);
+                if((item1 + item2) % 12 == 0){
+                    nAandB += 1;
+                }
+            }
+        }
 
-//      2-2. 합집합
-        a.addAll(b);
-        System.out.println(a);
-//      2-3. 차집합
+        System.out.println("결과" + (nA + nB - nAandB));
 
+        // HashSet 이용
+        HashSet<ArrayList> allCase = new HashSet<>();
+        for(int item1: dice1){
+            for(int item2: dice2){
+                if((item1 + item2) % 3 == 0 || (item1 + item2) % 4 == 0){
+                    ArrayList list = new ArrayList(Arrays.asList(item1, item2));
+                    allCase.add(list);
+                }
+            }
+        }
+        System.out.println("결과 : " + allCase.size());
+
+
+//      2. 곱의 법칙
+        System.out.println("== 곱의 법칙 ==");
+//      두 개의 주사위 a, b를 던졌을 때 a는 3의 배수, b는 4의 배수인 경우의 수
+        nA = 0;
+        nB = 0;
+
+        for(int item1 : dice1){
+            if(item1 % 3 == 0){
+                nA++;
+            }
+        }
+
+        for(int item2 : dice2){
+            if(item2 % 4 == 0){
+                nB++;
+            }
+        }
+
+        System.out.println("결과 : " + (nA * nB));
 
     }
-
 }
--- 코딩예제/JAVA/src/Practice.java
+++ 코딩예제/JAVA/src/Practice.java
@@ -1,114 +1,74 @@
 // Practice
-// ArrayList를 사용한 집합 구현 실습 (집합 관련 연산 사용 X)
+// 약수 구하기, 두 수의 최대공약수와 최소공배수 구하기
+// 활용) 1~10의 수 중 A의 약수 또는 B의 약수인 경우의 수
+// 활용) 1~10의 수 중 A의 약수이면서 B의 약수인 경우의 수
 
 import java.util.ArrayList;
 
-class MySet {
-// ArrayList
-    ArrayList<Integer> list;
-
-// 생성자1
-    MySet() {
-        this.list = new ArrayList<Integer>();
-    }
-
-// 생성자 2
-
-    MySet(int[] arr){
-        this.list = new ArrayList<Integer>();
-
-        for(int item:arr){
-            this.list.add(item);
-        }
-    }
-
-// 원소 추가 (중복 X)
-    public void add(int x){
-        for(int item: this.list){
-            if(item == x){
-                return;
-            }
-        }
-        this.list.add(x);
-    }
-
-// 교집합
-    public MySet retainAll(MySet b){
-        MySet result = new MySet();
-
-        for(int itemA: this.list){
-            for(int itemB: b.list){
-                if(itemA == itemB){
-                    result.add(itemA);
-                }
-            }
-        }
-        return result;
-    }
-
-// 합집합
-
-    public MySet addAll(MySet b){
-        MySet result = new MySet();
-
-        for(int itemA : this.list){
-            result.add(itemA);
-        }
-        for(int itemB : b.list){
-            result.add(itemB);
-        }
-        return result;
-
-    }
-
-// 차집합
-    public MySet removeAll(MySet b){
-        MySet result = new MySet();
-
-        for(int itemA:this.list){
-            boolean containFlag = false;
-
-            for(int itemB: b.list){
-                if(itemA == itemB){
-                    containFlag = true;
-                    break;
-                }
-            }
-            if(!containFlag){
-                result.add(itemA);
-            }
-        }
-        return result;
-    }
-
-}
-
 public class Practice {
+    
+//  약수
+    public ArrayList getDivisor(int num) {
+        ArrayList result = new ArrayList();
+
+        for (int i = 1; i <= (int)num/2; i++) {
+            if(num % i == 0){
+                result.add(i);
+            }
+        }
+        result.add(num);
+
+        return result;
+    }
+
+//  최대 공약수
+//  GCD: the Greatest Common Denominator
+    public int getGCD(int numA, int numB) {
+        int gcd = -1;
+
+        ArrayList divisorA = this.getDivisor(numA);
+        ArrayList divisorB = this.getDivisor(numB);
+
+        for(int itemA : (ArrayList<Integer>)divisorA){
+            for(int itemB : (ArrayList<Integer>)divisorB){
+                if(itemA == itemB){
+                    if(itemA > gcd){
+                        gcd = itemA;
+                    }
+                }
+            }
+        }
+
+        return gcd;
+    }
+
+//  최소 공배수
+//  LCM: the Lowest Common Multiple
+    public int getLCM(int numA, int numB) {
+        int lcm = -1;
+
+        int gcd = this.getGCD(numA, numB);
+
+        if(gcd != -1){
+            lcm = numA * numB / gcd;
+        }
+
+        return lcm;
+    }
+
     public static void main(String[] args) {
 
 //      Test code
-        MySet a = new MySet();
+        int number1 = 10;
+        int number2 = 6;
 
-        a.add(1);
-        a.add(1);
-        a.add(1);
-        System.out.println(a.list);
-        a.add(2);
-        a.add(3);
-        System.out.println(a.list);
+        Practice p = new Practice();
+        ArrayList l1 = p.getDivisor(number1);   // 10: 1, 2, 5, 10
+        ArrayList l2 = p.getDivisor(number2);   // 6: 1, 2, 3, 6
+        System.out.println("l1 = " + l1);
+        System.out.println("l2 = " + l2);
 
-        a = new MySet(new int[]{1, 2, 3, 4, 5});
-        MySet b = new MySet(new int[]{2, 4, 6, 8, 10});
-        System.out.println("a: " + a.list);
-        System.out.println("b: " + b.list);
-
-        MySet result = a.retainAll(b);
-        System.out.println("교집합: " + result.list);
-
-        result = a.addAll(b);
-        System.out.println("합집합: " + result.list);
-
-        result = a.removeAll(b);
-        System.out.println("차집합: " + result.list);
+        System.out.println("최대 공약수: " + p.getGCD(number1, number2));
+        System.out.println("최대 공배수: " + p.getLCM(number1, number2));
     }
 }
