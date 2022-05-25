9ae415d9bcc3fe7120a1945d21daf655e4dbda82
Jiyong Sim <sjy19910222@gmail.com>
2022-05-25 15:15:19 +0900

    Part02 자료구조/알고리즘 04.조합 추가

--- 코딩예제/JAVA/src/Main.java
+++ 코딩예제/JAVA/src/Main.java
@@ -1,62 +1,51 @@
-// 기초 수학 - 순열
-
-import java.util.stream.IntStream;
+// 기초 수학 - 조합
 
 public class Main {
+
+    static int getCombination(int n, int r){
+        int pResult = 1;
+        for (int i = n; i >= n - r +1; i--) {
+            pResult *= i ;
+        }
+
+
+        int rResult = 1;
+        for (int i = 1; i <= r; i++) {
+            rResult *= i ;
+        }
+        return pResult / rResult;
+    }
+
     public static void main(String[] args) {
+//      1. 조합
+        System.out.println("== 조합 ==");
 
-//      1. 팩토리얼
-        System.out.println("== 팩토리얼 ==");
-//      5!
-        int n = 5;
-        int result = 1;
+        int n = 4;
+        int r = 2;
 
-        for (int i = n; i <= n ; i++) {
-            result *= i;
+
+        int pResult = 1;
+        for (int i = n; i >= n - r +1; i--) {
+            pResult *= i ;
         }
-        System.out.println("result = " + result);
-        System.out.println(IntStream.range(2,6).reduce(1,(x ,y)->(x*y)));
 
 
-
-//      2. 순열
-        System.out.println("== 순열 ==");
-//      5명을 3줄로 세우는 경우의 수
-        n = 5;
-        int r = 3;
-
-        for (int i = n; i >= n -r + 1; i--) {
-            result *= i;
+        int rResult = 1;
+        for (int i = 1; i <= r; i++) {
+            rResult *= i ;
         }
-        System.out.println("result = " + result);
-
-
-//      3. 중복 순열
-        System.out.println("== 중복 순열 ==");
-//      서로 다른 4개의 수 중 2개를 뽑는 경우의 수 (중복 허용)
-        n = 4;
-        r = 2;
-        result = 1;
-
-
-        for (int i = 0; i < r; i++) {
-            result *= n;
-        }
-        System.out.println(result);
-
-        System.out.println(Math.pow(n,r));
+        System.out.println("결과 : " + pResult / rResult);
 
 
 
-//      4. 원 순열
-        System.out.println("== 원 순열 ==");
-//      원 모양의 테이블에 3명을 앉히는 경우의 수
+//      2. 중복 조합
+        System.out.println("== 중복 조합 ==");
+        n = 2;
+        r = 3;
 
-        n = 3;
-        result = 1;
-        for (int i = 1; i < n; i++) {
-            result *= i;
-            System.out.println("result = " + result);
-        }
+
+        System.out.println("결과 : " + getCombination(n + r - 1, r));
+
+
     }
 }
