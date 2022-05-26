5a38d4424c6da04e72480cd70a2084cfab54dfdc
Jiyong Sim <sjy19910222@gmail.com>
2022-05-26 12:22:10 +0900

    Part02 자료구조/알고리즘 06.점화식과 재귀함수 추가

--- 코딩예제/JAVA/src/Main.java
+++ 코딩예제/JAVA/src/Main.java
@@ -1,51 +1,73 @@
-// 기초 수학 - 조합
+// 기초 수학 - 점화식과 재귀함수
 
 public class Main {
 
-    static int getCombination(int n, int r){
-        int pResult = 1;
-        for (int i = n; i >= n - r +1; i--) {
-            pResult *= i ;
+    static int recursion1(int n){
+        if(n == 1){
+            return 1;
         }
+        return 3 * recursion1(n -1);
+    }
 
-
-        int rResult = 1;
-        for (int i = 1; i <= r; i++) {
-            rResult *= i ;
+    static int recursion2(int n){
+        if(n == 1){
+            return 1;
         }
-        return pResult / rResult;
+        return n + recursion2(n - 1);
+    }
+
+    static int recursion3(int n){
+        if(n < 3){
+            return 1;
+        }
+        return recursion3(n -2) + recursion3(n -1);
     }
 
     public static void main(String[] args) {
-//      1. 조합
-        System.out.println("== 조합 ==");
 
+//      점화식 -> 반복문, 재귀함수
+        System.out.println("== 점화식/재귀함수 연습1 ==");
+//      1, 3, 9, 27, ... 의 n번째 수
         int n = 4;
-        int r = 2;
-
-
-        int pResult = 1;
-        for (int i = n; i >= n - r +1; i--) {
-            pResult *= i ;
+        int result = 1;
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                result = 1;
+            } else {
+                result *= 3;
+            }
         }
+        System.out.println(result);
+        System.out.println("재귀함수 결과: "+recursion1(4));
 
 
-        int rResult = 1;
-        for (int i = 1; i <= r; i++) {
-            rResult *= i ;
+        System.out.println("== 점화식/재귀함수 연습2 ==");
+//      1, 2, 3, 4, 5, 6, ... 의 n번째 까지의 합
+        n = 5;
+        result = 0;
+        for (int i = 1; i < n + 1; i++) {
+            result += i;
         }
-        System.out.println("결과 : " + pResult / rResult);
+        System.out.println(result);
+        System.out.println("재귀함수 결과: "+ recursion2(5));
 
+        System.out.println("== 점화식/재귀함수 연습3 ==");
+//      1, 1, 2, 3, 5, 8, 13, ...의 n번 째 수
+        n = 7;
+        result = 0;
+        int a1 = 1;
+        int a2 = 1;
 
-
-//      2. 중복 조합
-        System.out.println("== 중복 조합 ==");
-        n = 2;
-        r = 3;
-
-
-        System.out.println("결과 : " + getCombination(n + r - 1, r));
-
+        if(n < 3){
+            result = 1;
+        }else {
+            for (int i = 2; i < n; i++) {
+                result = a1 + a2;
+                a1 = a2;
+                a2 = result;
+            }
+        }
+        System.out.println(result);
 
     }
 }
