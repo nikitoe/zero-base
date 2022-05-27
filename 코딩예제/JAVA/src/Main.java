fc429d13dc83e6da20b14f0c711444801cd60e39
Jiyong Sim <sjy19910222@gmail.com>
2022-05-27 16:38:28 +0900

    Part02 자료구조/알고리즘 08.알고리즘 복잡도 추가

--- 코딩예제/JAVA/src/Main.java
+++ 코딩예제/JAVA/src/Main.java
@@ -1,36 +1,78 @@
-// 기초 수학 - 지수와 로그
+// 기초 수학 - 알고리즘 복잡도
 
 public class Main {
+    static int fibonacci(int n) {
+        if (n < 3) {
+            return 1;
+        }
+        return fibonacci(n - 2) + fibonacci(n - 1);
+    }
+
+    static int factorial(int n) {
+        if (n < 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
 
     public static void main(String[] args) {
 
-//      1. 제곱, 제곱근, 지수
-        System.out.println("== 제곱 ==");
-        System.out.println(Math.pow(2,3));
-        System.out.println(Math.pow(2,-3));
-        System.out.println(Math.pow(-2,-3));
-        System.out.println(Math.pow(2,30));
-        System.out.printf("%.0f\n", Math.pow(2,30));
+//      1. 시간 복잡도
+        System.out.println("== 시간 복잡도 ==");
 
-        System.out.println("== 제곱근 ==");
-        System.out.println(Math.sqrt(16));
-        System.out.println(Math.pow(16,1.0/2));
-        System.out.println(Math.pow(16,1.0/4));
+//      O(1)
+        System.out.println("== O(1) ==");
+        System.out.println("hello");
+
+//      O(logN)
+        System.out.println("== O(logN) ==");
+        for (int i = 1; i < 16; i*=2) {
+            System.out.println("hello");
+        }
+
+//      O(N)
+        System.out.println("== O(N) ==");
+        for (int i = 0; i < 2; i++) {
+            System.out.println("hello");
+        }
+
+//      O(NlogN)
+        System.out.println("== O(NlogN) ==");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 1; j < 8; j*=2) {
+                System.out.println("hello");
+            }
+        }
+
+//      O(N^2)
+        System.out.println("== O(N^2) ==");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print("hello ");
+            }
+            System.out.println();
+        }
+
+//      O(2^N)
+        System.out.println("== O(2^N) ==");
+//      피보나치 재귀
+//      1, 1, 2, 3, 5, 8, 13, ...
+        System.out.println(fibonacci(6));
 
 
-//      참고) 절대 값
-        System.out.println("== 절대 값 ==");
-        System.out.println(Math.abs(5));
-        System.out.println(Math.abs(-5));
+//      2. 공간 복잡도
+        System.out.println("== 공간 복잡도 ==");
+//      O(N)
+        System.out.println("== O(N) ==");
+        int n = 3;
+        System.out.println(factorial(n));
 
-
-//      2. 로그
-        System.out.println("== 로그 ==");
-        System.out.println(Math.E);
-        System.out.println(Math.log(2.718281828459045));
-        System.out.println(Math.log10(1000));
-        System.out.println(Math.log(4)/Math.log(2));
-
-
+//      O(1)
+        System.out.println("== O(1) ==");
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        System.out.println(result);
     }
 }
