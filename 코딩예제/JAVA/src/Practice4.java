a6cf2b7ecaec80b8f0caf7c6b5bd91f7b781d94a
Jiyong Sim <sjy19910222@gmail.com>
2022-05-30 23:55:01 +0900

    Part02 자료구조/알고리즘 연습 문제 풀이 4 추가

--- 코딩예제/JAVA/src/Practice4.java
+++ 코딩예제/JAVA/src/Practice4.java
@@ -2,6 +2,23 @@ import java.util.HashSet;
 
 public class Practice4 {
     public static boolean solution(int n) {
+        HashSet<Integer> set = new HashSet<>();
+
+        while (set.add(n)){
+            int sqaureSum = 0;
+
+            while (n > 0){
+                int remain = n % 10;
+                sqaureSum = remain * remain;
+                n/= 10;
+            }
+
+            if(sqaureSum == 1){
+                return true;
+            } else {
+                n = sqaureSum;
+            }
+        }
 
         return false;
     }
