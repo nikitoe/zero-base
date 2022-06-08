20e5299f178207219e73d1e3cc795dbbf9cb6be4
Jiyong Sim <sjy19910222@gmail.com>
2022-06-08 23:45:04 +0900

    Part02 자료구조/알고리즘 02. 선형 자료구조-큐

--- 코딩예제/JAVA/src/Practice2.java
+++ 코딩예제/JAVA/src/Practice2.java
@@ -1,92 +1,49 @@
 // Practice2
-// 배열을 이용한 기본 스택 직접 구현
+// 요세푸스 문제
+// N과 K가 주어졌을 때 (N, K) 요세푸스 순열을 구하시오.
+// N과 K는 N >= K 를 만족하는 양의 정수이다.
+// 1부터 N 번까지 N명이 순서대로 원을 이루어 모여 있다.
+// 이 모임에서 원을 따라 순서대로 K번째 사람을 제외한다.
+// 모든 사람이 제외될 때까지 반복하며 이 때, 제외되는 순서가 요세푸스 순열이다.
 
-class MyStack2 {
-    int[] arr;
-    int top = -1;
+// 예시 입력
+// 입력: N = 5, K = 2
+// 결과: 2, 4, 1, 5, 3
 
-    MyStack2(int size) {
-        arr = new int[size];
-    }
+// 입력: N = 7, K = 3
+// 결과: 3, 6, 2, 7, 5, 1, 4
 
-    public boolean isEmpty() {
-        if (this.top == -1) {
-            return true;
-        } else {
-            return false;
-        }
-    }
 
-    public boolean isFull() {
-        if (this.top == this.arr.length - 1) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public void push(int data) {
-        if (this.isFull()) {
-            System.out.println("Stack is full!");
-            return;
-        }
-
-        this.top += 1;
-        this.arr[this.top] = data;
-    }
-
-    public Integer pop() {
-        if (this.isEmpty()) {
-            System.out.println("Stack is empty!");
-            return null;
-        }
-
-        int data = this.arr[this.top];
-        this.top -= 1;
-
-        return data;
-    }
-
-    public Integer peek() {
-        if (this.isEmpty()) {
-            System.out.println("Stack is empty!");
-            return null;
-        }
-
-        return this.arr[this.top];
-    }
-
-    public void printStack() {
-        for (int i = 0; i < this.top + 1; i++) {
-            System.out.print(this.arr[i] + " ");
-        }
-        System.out.println();
-    }
-}
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.stream.IntStream;
 
 public class Practice2 {
+
+    public static ArrayList getJosephusPermutation(int N, int K) {
+        Queue queue = new LinkedList();
+        ArrayList result = new ArrayList();
+
+        IntStream.range(1, N + 1).forEach(x -> queue.add(x));
+
+        int cnt = 0;
+        while (!queue.isEmpty()) {
+            int data = (int)queue.remove();
+            cnt += 1;
+            if (cnt % K == 0) {
+                result.add(data);
+            } else {
+                queue.add(data);
+            }
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
         // Test code
-        MyStack2 myStack = new MyStack2(5);
-        myStack.isEmpty();
-        myStack.push(1);
-        myStack.push(2);
-        myStack.push(3);
-        myStack.push(4);
-        myStack.push(5);
-        myStack.push(6);
-        myStack.printStack();               // 1, 2, 3, 4, 5
-
-        System.out.println(myStack.peek()); // 5
-        myStack.printStack();               // 1, 2, 3, 4, 5
-
-        System.out.println(myStack.pop());  // 5
-        System.out.println(myStack.pop());  // 4
-        myStack.printStack();               // 1, 2, 3
-
-        System.out.println(myStack.pop());  // 3
-        System.out.println(myStack.pop());  // 2
-        System.out.println(myStack.pop());  // 1
-        myStack.printStack();
+        System.out.println(getJosephusPermutation(5, 2));
+        System.out.println(getJosephusPermutation(7, 3));
     }
 }
