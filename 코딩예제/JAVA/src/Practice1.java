23236bde03207f75e47c3a3b7bf5873244431cd1
Jiyong Sim <sjy19910222@gmail.com>
2022-06-09 14:28:21 +0900

    Part02 자료구조/알고리즘 02. 선형 자료구조-데크

--- 코딩예제/JAVA/src/Practice1.java
+++ 코딩예제/JAVA/src/Practice1.java
@@ -1,44 +1,87 @@
 // Practice1
-// 카드 섞기
-// 1부터 N 까지의 번호로 구성된 N장의 카드가 있다.
-// 1번 카드가 가장 위에 그리고 N번 카드는 가장 아래의 상태로 카드가 순서대로 쌓여있다.
-// 아래의 동작을 카드 한 장만 남을 때까지 반복했을 때, 가장 마지막 남는 카드 번호를 출력하시오.
-// 1. 가장 위의 카드는 버린다.
-// 2. 그 다음 위의 카드는 쌓여 있는 카드의 가장 아래에 다시 넣는다.
+// ArrayList 를 이용한 데크 구현
 
-// 예시 입력)
-// N = 4
-// 결과: 4
+import java.util.ArrayList;
 
-// N = 7
-// 결과: 6
+class MyDeque1 {
+    ArrayList list;
 
-
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.stream.IntStream;
-
-public class Practice1 {
-    public static int findLastCard(int N) {
-        Queue queue = new LinkedList();
-
-        IntStream.range(1, N + 1).forEach(x -> queue.add(x));
-        System.out.println(queue);
-
-        while (queue.size() > 1) {
-            queue.remove();
-            int data = (int)queue.remove();
-            queue.add(data);
-            System.out.println(queue);
-        }
-
-        return (int)queue.remove();
+    MyDeque1() {
+        this.list = new ArrayList();
     }
 
+    public boolean isEmpty() {
+        if (this.list.size() == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void addFirst(int data) {
+        this.list.add(0, data);
+    }
+
+    public void addLast(int data) {
+        this.list.add(data);
+    }
+
+    public Integer removeFirst() {
+        if (this.isEmpty()) {
+            System.out.println("Deque is empty!");
+            return null;
+        }
+
+        int data = (int)this.list.get(0);
+        this.list.remove(0);
+        return data;
+    }
+
+    public Integer removeLast() {
+        if (this.isEmpty()) {
+            System.out.println("Deque is empty!");
+            return null;
+        }
+
+        int data = (int)this.list.get(this.list.size() - 1);
+        this.list.remove(this.list.size() - 1);
+        return data;
+    }
+
+    public void printDeque() {
+        System.out.println(this.list);
+    }
+
+}
+
+public class Practice1 {
     public static void main(String[] args) {
         // Test code
-        System.out.println(findLastCard(4));    // 4
-        System.out.println(findLastCard(7));    // 6
-        System.out.println(findLastCard(9));    // 2
+        MyDeque1 myDeque = new MyDeque1();
+        // Front 부분 입력
+        myDeque.addFirst(1);
+        myDeque.addFirst(2);
+        myDeque.addFirst(3);
+        myDeque.printDeque();    // 3 2 1
+
+        // Rear 부분 입력
+        myDeque.addLast(10);
+        myDeque.addLast(20);
+        myDeque.addLast(30);
+        myDeque.printDeque();    // 3 2 1 10 20 30
+
+        // Front 부분 출력
+        System.out.println(myDeque.removeFirst());  // 3
+        myDeque.printDeque();    // 2 1 10 20 30
+
+        // Rear 부분 출력
+        System.out.println(myDeque.removeLast());   // 30
+        myDeque.printDeque();    // 2 1 10 20
+
+        System.out.println(myDeque.removeLast());   // 20
+        System.out.println(myDeque.removeLast());   // 10
+        System.out.println(myDeque.removeLast());   // 1
+        System.out.println(myDeque.removeLast());   // 2
+        myDeque.printDeque();
     }
 }
--- 코딩예제/JAVA/src/Practice2.java
+++ 코딩예제/JAVA/src/Practice2.java
@@ -1,49 +1,103 @@
 // Practice2
-// 요세푸스 문제
-// N과 K가 주어졌을 때 (N, K) 요세푸스 순열을 구하시오.
-// N과 K는 N >= K 를 만족하는 양의 정수이다.
-// 1부터 N 번까지 N명이 순서대로 원을 이루어 모여 있다.
-// 이 모임에서 원을 따라 순서대로 K번째 사람을 제외한다.
-// 모든 사람이 제외될 때까지 반복하며 이 때, 제외되는 순서가 요세푸스 순열이다.
+// 배열을 이용한 기본 데크 직접 구현
 
-// 예시 입력
-// 입력: N = 5, K = 2
-// 결과: 2, 4, 1, 5, 3
+class MyDeque2 {
+    int[] arr;
+    int front = 0;
+    int rear = 0;
 
-// 입력: N = 7, K = 3
-// 결과: 3, 6, 2, 7, 5, 1, 4
-
-
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.stream.IntStream;
-
-public class Practice2 {
-
-    public static ArrayList getJosephusPermutation(int N, int K) {
-        Queue queue = new LinkedList();
-        ArrayList result = new ArrayList();
-
-        IntStream.range(1, N + 1).forEach(x -> queue.add(x));
-
-        int cnt = 0;
-        while (!queue.isEmpty()) {
-            int data = (int)queue.remove();
-            cnt += 1;
-            if (cnt % K == 0) {
-                result.add(data);
-            } else {
-                queue.add(data);
-            }
-        }
-
-        return result;
+    MyDeque2(int size) {
+        this.arr = new int[size + 1];
     }
 
+    public boolean isEmpty() {
+        return this.rear == this.front;
+    }
+
+    public boolean isFull() {
+        return (this.rear + 1)  % this.arr.length == this.front;
+    }
+
+    public void addFirst(int data) {
+        if (this.isFull()) {
+            System.out.println("Deque is full!");
+            return;
+        }
+
+        this.arr[front] = data;
+        this.front = (this.front - 1 + this.arr.length) % this.arr.length;
+    }
+
+    public void addLast(int data) {
+        if (this.isFull()) {
+            System.out.println("Deque is full!");
+            return;
+        }
+
+        this.rear = (this.rear + 1) % this.arr.length;
+        this.arr[this.rear] = data;
+    }
+
+    public Integer removeFirst() {
+        if (this.isEmpty()) {
+            System.out.println("Deque is empty!");
+            return null;
+        }
+
+        this.front = (this.front + 1) % this.arr.length;
+        return this.arr[this.front];
+    }
+
+    public Integer removeLast() {
+        if (this.isEmpty()) {
+            System.out.println("Deque is empty!");
+            return null;
+        }
+
+        int data = this.arr[this.rear];
+        this.rear = (this.rear - 1 + this.arr.length) % this.arr.length;
+        return data;
+    }
+
+    public void printDeque() {
+        int start = (this.front + 1) % this.arr.length;
+        int end = (this.rear + 1) % this.arr.length;
+
+        for (int i = start; i != end; i = (i + 1) % this.arr.length) {
+            System.out.print(this.arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+}
+
+public class Practice2 {
     public static void main(String[] args) {
         // Test code
-        System.out.println(getJosephusPermutation(5, 2));
-        System.out.println(getJosephusPermutation(7, 3));
+        MyDeque2 myDeque = new MyDeque2(5);
+        // Front 부분 입력
+        myDeque.addFirst(1);
+        myDeque.addFirst(2);
+        myDeque.addFirst(3);
+        myDeque.printDeque();   // 3 2 1
+
+        // Rear 부분 입력
+        myDeque.addLast(10);
+        myDeque.addLast(20);
+        myDeque.addLast(30);    // Deque is full!
+        myDeque.printDeque();        // 3 2 1 10 20
+
+        // Front 부분 출력
+        System.out.println(myDeque.removeFirst());  // 3
+        myDeque.printDeque();   // 2 1 10 20
+
+        // Rear 부분 출력
+        System.out.println(myDeque.removeLast());   // 20
+        myDeque.printDeque();    // 2 1 10
+
+        System.out.println(myDeque.removeLast());   // 10
+        System.out.println(myDeque.removeLast());   // 1
+        System.out.println(myDeque.removeLast());   // 2
+        System.out.println(myDeque.removeLast());   // Deque is empty!
     }
 }
