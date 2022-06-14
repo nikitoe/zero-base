86ea11ecc87b7c930c82949e70adefa419cfdb0c
Jiyong Sim <sjy19910222@gmail.com>
2022-06-15 02:49:57 +0900

    art02 자료구조/알고리즘 03. 비선형 자료구조- 이진 트리

--- 코딩예제/JAVA/src/Practice1.java
+++ 코딩예제/JAVA/src/Practice1.java
@@ -1,54 +1,104 @@
 // Practice1
-// 종이 접기
-// 종이를 반으로 접었을 때, 안으로 파인 부분은 0, 볼록 튀어나온 부분은 1이라고 하자.
-// 종이를 접을 때는 오른쪽에서 왼쪽으로 접는다.
-// 종이를 N번 접었을 때의 접힌 상태를 출력하는 문제를 작성하세요.
+// 앞의 BST 삽입, 삭제 코드를 재귀 형태로 구현
 
-// 입출력 예시
-// 입력: 1
-// 출력: 0
+import java.util.LinkedList;
+import java.util.Queue;
 
-// 입력: 2
-// 출력: 0, 0, 1
+class BinarySearchTree2 {
+    Node head;
 
-// 입력: 3
-// 출력: 0, 0, 1, 0, 0, 1, 1
+    BinarySearchTree2(int key) {
+        this.head = new Node(key, null, null);
+    }
+
+    public Node addNodeRecursive(Node cur, int key) {
+        if (cur == null) {
+            return new Node(key, null, null);
+        }
+
+        if (key < cur.key) {
+            cur.left = addNodeRecursive(cur.left, key);
+        } else {
+            cur.right = addNodeRecursive(cur.right, key);
+        }
+
+        return cur;
+    }
+
+    public Node removeNodeRecursive(Node cur, int key) {
+        if (cur == null) {
+            return null;
+        }
+
+        if (key < cur.key) {
+            cur.left = removeNodeRecursive(cur.left, key);
+        } else if (key > cur.key) {
+            cur.right = removeNodeRecursive(cur.right, key);
+        } else {
+            if (cur.left == null) {
+                return cur.right;
+            } else if (cur.right == null) {
+                return cur.left;
+            } else {
+                Node predecessor = cur;
+                Node successor = cur.left;
+
+                while (successor.right != null) {
+                    predecessor = successor;
+                    successor = successor.right;
+                }
+
+                predecessor.right = successor.left;
+                cur.key = successor.key;
+            }
+        }
+
+        return cur;
+    }
+
+    public void levelOrder(Node node) {
+        Queue<Node> queue = new LinkedList();
+        queue.add(node);
+        while (!queue.isEmpty()) {
+            Node cur = queue.poll();
+
+            System.out.print(cur.key + " ");
+            if (cur.left != null) {
+                queue.offer(cur.left);
+            }
+
+            if (cur.right != null) {
+                queue.offer(cur.right);
+            }
+        }
+        System.out.println();
+    }
+
+}
 
 
 public class Practice1 {
-    public static void solution(int n) {
-        int[] binaryTree = new int[(int)Math.pow(2, n)];
-        
-        binaryTree[0] = 0;
-        for (int i = 0; i < (int)Math.pow(2, n -1) - 1; i++) {
-            binaryTree[i * 2 + 1] = 0;
-            binaryTree[i * 2 + 2] = 1;
-        }
-
-        inOrder(binaryTree, 0);
-        System.out.println();
-
-    }
-    public static void inOrder(int[] arr, int idx){
-        int left = 2 * idx + 1;
-        int right = 2 * idx + 2;
-
-        if(left < arr.length - 1){
-            inOrder(arr, left);
-        }
-
-        System.out.print(arr[idx] + " ");
-
-        if(right < arr.length - 1){
-            inOrder(arr, right);
-        }
-    }
-
-
     public static void main(String[] args) {
         // Test code
-        solution(1);
-        solution(2);
-        solution(3);
+        // 노드 삽입
+        BinarySearchTree2 bst = new BinarySearchTree2(20);
+        bst.head = bst.addNodeRecursive(bst.head, 10);
+        bst.head = bst.addNodeRecursive(bst.head, 30);
+        bst.head = bst.addNodeRecursive(bst.head, 1);
+        bst.head = bst.addNodeRecursive(bst.head, 15);
+        bst.head = bst.addNodeRecursive(bst.head, 25);
+        bst.head = bst.addNodeRecursive(bst.head, 13);
+        bst.head = bst.addNodeRecursive(bst.head, 35);
+        bst.head = bst.addNodeRecursive(bst.head, 27);
+        bst.head = bst.addNodeRecursive(bst.head, 40);
+        bst.levelOrder(bst.head);
+
+        // 노드 삭제
+        bst.head = bst.removeNodeRecursive(bst.head,40);
+        bst.levelOrder(bst.head);
+        bst.head = bst.removeNodeRecursive(bst.head, 25);
+        bst.levelOrder(bst.head);
+        bst.head = bst.removeNodeRecursive(bst.head, 20);
+        bst.levelOrder(bst.head);
     }
 }
