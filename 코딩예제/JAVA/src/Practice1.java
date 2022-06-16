c16d8a59e26ce0ce600bf875877b33d652335725
Jiyong Sim <sjy19910222@gmail.com>
2022-06-16 12:06:24 +0900

    art02 자료구조/알고리즘 03. 비선형 자료구조- 이진 트리(Red-Black 트리)

--- 코딩예제/JAVA/src/Practice1.java
+++ 코딩예제/JAVA/src/Practice1.java
@@ -1,121 +1,181 @@
-// 비선형 자료구조 - 이진 탐색 트리_2
-// AVL 트리 - 삽입
+// 비선형 자료구조 - 이진 탐색 트리_3
+// Red-Black 트리 - 삽입
 
 import java.util.LinkedList;
 import java.util.Queue;
 
 class Node {
     int key;
-    int height;
+    int color;
     Node left;
     Node right;
+    Node parent;
 
-    public Node(int key, Node left, Node right) {
+    public Node(int key, int color, Node left, Node right, Node parent) {
         this.key = key;
-        this.height = 0;
+        this.color = color;
         this.left = left;
         this.right = right;
+        this.parent = parent;
     }
 }
 
-class AVLTree {
+class RedBlackTree{
+    final int BLACK = 0;
+    final int RED = 1;
+
     Node head;
 
-    public int height(Node node) {
-        if (node == null) {
-            return -1;
+    public  void insert(int key) {
+        Node checkNode = null;
+        if (this.head == null) {
+            this.head = new Node(key, BLACK, null,null,null);
+        } else {
+            Node cur = this.head;
+
+            while (true) {
+                Node pre = cur;
+
+                if (key < cur.key) {
+                    cur = cur.left;
+                    if (cur == null) {
+                        pre.left = new Node(key, RED, null, null, pre);
+                        checkNode = pre.left;
+                        break;
+                    }
+                } else {
+                    cur = cur.right;
+
+                    if (cur ==null) {
+                        pre.right = new Node(key, RED, null, null,pre);
+                        checkNode = pre.right;
+                        break;
+                    }
+                }
+            }
+            reBalance(checkNode);
+
         }
-        return node.height;
+    }
+    public void reBalance(Node node) {
+        while (node.parent != null && node.parent.color == RED) {
+            Node sibling = null;
+
+            // 형제 노드 잡기
+           if (node.parent == node.parent.parent.left) {
+                sibling = node.parent.parent.right;
+            } else {
+                sibling = node.parent.parent.left;
+            }
+
+           // 부모 노드의 형제 노드가 RED인 경우
+            if (sibling != null && sibling.color == RED) {
+                node.parent.color = BLACK;
+                sibling.color = BLACK;
+                node.parent.parent.color = RED;
+
+                if (node.parent.parent == this.head) {
+                    node.parent.parent.color = BLACK;
+                    break;
+                } else {
+                    node = node.parent.parent;
+                    continue;
+                }
+            } else {
+                // 부모 노드의 형제가 없거나, BLACK인 경우, Restructuring 진행
+                if (node.parent == node.parent.parent.left) {
+
+                    // LR인 경우, LL로 만들기
+                    if (node == node.parent.right) {
+                        node = node.parent;
+
+                        // 회전
+                        leftRotate(node);
+                    }
+                    node.parent.color = BLACK;
+                    node.parent.parent.color = RED;
+                    rightRotate(node.parent.parent);
+
+                } else if (node.parent == node.parent.parent.right) {
+                    if (node == node.parent.left) {
+                        node = node.parent;
+                        // 회전
+                        rightRotate(node);
+                    }
+                    node.parent.color = BLACK;
+                    node.parent.parent.color = RED;
+                    leftRotate(node.parent.parent);
+                }
+                break;
+            }
+        }
     }
 
-    public Node rightRotate(Node node) {
-        Node lNode = node.left;
+    public void leftRotate(Node node) {
+        if (node.parent == null) {
+            Node rNode = this.head.right;
+            this.head.right = rNode.left;
+            rNode.left.parent = this.head;
+            this.head.parent = rNode;
+            rNode.left = this.head;
+            rNode.parent = null;
+            this.head = rNode;
 
-        node.left = lNode.right;
-        lNode.right = node;
-        node.height = Math.max(height(node.left),height(node.right)) + 1;
-        lNode.height = Math.max(height(lNode.left),height(lNode.right)) + 1;
+        } else {    // head가 아닌 경우
+           if ( node == node.parent.left) {
+               node.parent.left = node.right;
+            } else {
+               node.parent.right = node.right;
+           }
+           node.right.parent = node.parent;
+           node.parent = node.right;
 
-        return lNode;
+            if (node.right.left != null) {
+                node.right.left.parent = node;
+            }
+
+            node.right = node.right.left;
+            node.parent.left = node;
+        }
     }
 
-    public Node leftRotate(Node node) {
-        Node rNode = node.right;
+    public void rightRotate(Node node) {
+        if (node.parent == null) {
+            Node lNode = this.head.left;
+            this.head.left = lNode.right;
+            lNode.right.parent = this.head;
+            this.head.parent = lNode;
+            lNode.right = this.head;
+            lNode.parent = null;
+            this.head = lNode;
 
-        node.right = rNode.left;
-        rNode.left = node;
+        } else {    // head가 아닌 경우
+            if ( node == node.parent.left) {
+                node.parent.left = node.left;
+            } else {
+                node.parent.right = node.left;
+            }
+            node.left.parent = node.parent;
+            node.parent = node.left;
 
-        node.height = Math.max(height(node.left),height(node.right)) + 1;
-        rNode.height = Math.max(height(rNode.left),height(rNode.right)) + 1;
-        return rNode;
-    }
+            if (node.left.right != null) {
+                node.left.right.parent = node;
+            }
 
-    public Node lrRotate(Node node) {
-        node.left = leftRotate(node.left);
-        return rightRotate(node);
-    }
-
-    public Node rlRotate(Node node) {
-        node.right = rightRotate(node.right);
-        return leftRotate(node);
-    }
-
-    public int getBalance(Node node) {
-        if (node == null) {
-            return 0;
+            node.left = node.left.right;
+            node.parent.right = node;
         }
-
-        return height(node.left) - height(node.right);
-    }
-
-    public void insert(int key) {
-        this.head = insert(this.head ,key);
-    }
-
-    public Node insert(Node node, int key) {
-        if (node == null) {
-            return new Node(key, null, null);
-        }
-        if (key < node.key) {
-            node.left = insert(node.left, key);
-        }else {
-            node.right = insert(node.right, key);
-        }
-
-        node.height = Math.max(height(node.left),height(node.right)) + 1;
-
-        int balance = getBalance(node);
-
-        //LL
-        if (balance > 1 && key < node.left.key) {
-            return rightRotate(node);
-        }
-
-        //RR
-        if(balance < -1 && key > node.right.key){
-            return leftRotate(node);
-        }
-
-        //LR
-        if (balance > 1 && key > node.left.key) {
-            return lrRotate(node);
-        }
-
-        //RL
-        if (balance < -1 && key < node.right.key) {
-            return rlRotate(node);
-        }
-
-        return node;
     }
 
     public void levelOrder(Node node) {
-        Queue<Node> queue = new LinkedList();
+        char[] color = {'B', 'R'};
+
+        Queue<Node> queue = new LinkedList<>();
         queue.add(node);
         while (!queue.isEmpty()) {
             Node cur = queue.poll();
 
-            System.out.print(cur.key + " ");
+            System.out.print("[" + color[cur.color] + "]" + cur.key + " ");
             if (cur.left != null) {
                 queue.offer(cur.left);
             }
@@ -131,24 +191,17 @@ class AVLTree {
 public class Practice1 {
     public static void main(String[] args) {
         // Test code
-        AVLTree avl = new AVLTree();
-
-        // Insert
-        avl.insert(30);
-        avl.insert(20);
-        avl.insert(10);     // LL
-        avl.levelOrder(avl.head);
-
-        avl.insert(40);
-        avl.insert(50);     // RR
-        avl.levelOrder(avl.head);
-
-        avl.insert(5);
-        avl.insert(7);      // LR
-        avl.levelOrder(avl.head);
-
-        avl.insert(60);
-        avl.insert(55);     // RL
-        avl.levelOrder(avl.head);
+        RedBlackTree rbTree = new RedBlackTree();
+        rbTree.insert(20);
+        rbTree.insert(10);
+        rbTree.insert(30);
+        rbTree.levelOrder(rbTree.head);
+        rbTree.insert(25);
+        rbTree.levelOrder(rbTree.head);
+        rbTree.insert(5);
+        rbTree.insert(7);
+        rbTree.levelOrder(rbTree.head);
+        rbTree.insert(20);
+        rbTree.levelOrder(rbTree.head);
     }
 }
