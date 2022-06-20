b194eb716c02d2f52aa8e7755783f45c8fa62765
Jiyong Sim <sjy19910222@gmail.com>
2022-06-21 00:50:06 +0900

    part04_객체 지향/ 라이브러리 활용_배열

---
+++ Part04. 객체 지향 라이브러리 활용/JAVA/CardSample/src/배열테스트.java
@@ -0,0 +1,28 @@
+import java.util.Iterator;
+
+public class 배열테스트 {
+
+	public static void main(String[] args) {
+
+		int[] arr = new int[5];
+		arr[0] = 10;
+		arr[1] = 20;
+		arr[2] = 30;
+		arr[3] = 40;
+		arr[4] = 50;
+		
+		int[] arr1 = {10, 20, 30, 40, 50};
+
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+
+		}
+		
+		System.out.println("==========================");
+		for (int i = 0; i < arr1.length; i++) {
+			System.out.println(arr1[i]);
+
+		}
+		
+	}
+}
