50b80d8b6ab4471112867faf2eff39f67bb88c42
Jiyong Sim <sjy19910222@gmail.com>
2022-06-20 23:46:23 +0900

    part04_객체 지향/ 라이브러리 활용_변수를 상수로 만드는 final

---
+++ Part04. 객체 지향 라이브러리 활용/JAVA/CardSample/src/달력.java
@@ -0,0 +1,14 @@
+
+
+public class 달력 {
+	
+	//	final을 이용하여 값을 변경 못하게
+	//	객체가 만들어질 때,닥 한번만 값이 설정되고 값이 안 바뀔때는 final만 사용가능
+	//	public final int LAST_MONTH;
+	//	public 달력(int month) {
+	//	LAST_MONTH = month;
+	//	}
+	public static final int LAST_MONTH = 12;
+	
+
+}
---
+++ Part04. 객체 지향 라이브러리 활용/JAVA/CardSample/src/달력테스트.java
@@ -0,0 +1,13 @@
+
+public class 달력테스트 {
+
+	public static void main(String[] args) {
+		
+		//달력 내달력 = new 달력();
+		//내달력.lastMonth = 13;
+		
+		// static을 이용하여 객체를 만들지 않고 바로 사용 가능
+		System.out.println(달력.LAST_MONTH);
+	}
+
+}
