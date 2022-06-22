49ae80ef061702726876735bbfa6210e4ecffd3a
Jiyong Sim <sjy19910222@gmail.com>
2022-06-22 22:12:41 +0900

    Part10_다형성_오버라이딩

---
+++ Part04. 객체 지향 라이브러리 활용/JAVA/Exam03/src/sam01/문자열처리클래스.java
@@ -0,0 +1,7 @@
+package sam01;
+
+public class 문자열처리클래스 {
+
+	//String ----> 문자열 클래스...
+	//
+}
---
+++ Part04. 객체 지향 라이브러리 활용/JAVA/Exam03/src/sam06/합계계산테스트.java
@@ -0,0 +1,13 @@
+package sam06;
+
+public class 합계계산테스트{
+	
+	public static void main(String[] args) {
+		
+		합계계산 계산1 = new 합계계산();
+		
+		System.out.println(계산1.더하기(10, 20));
+		System.out.println(계산1.더하기(10, 20, 30));
+		System.out.println(계산1.더하기(new int[] {10, 20, 30, 40, 50}, 5));
+	}
+}
