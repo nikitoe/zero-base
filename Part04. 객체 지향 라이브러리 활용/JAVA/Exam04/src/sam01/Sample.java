fe359b7599783b3cdb237f39653c2c11312a95c7
Jiyong Sim <sjy19910222@gmail.com>
2022-06-26 21:10:33 +0900

    01_Object, String 클래스_java.lang패키지

---
+++ Part04. 객체 지향 라이브러리 활용/JAVA/Exam04/src/sam01/Sample.java
@@ -0,0 +1,19 @@
+package sam01;
+
+import java.util.Date;
+import java.util.Calendar;
+
+public class Sample {
+	public static void main(String[] args) {
+		
+		// java.lang 패키지로, import문 없이 사용 가능
+		String str = "홍길동";
+		System.out.println(str);
+		
+		// java.util 패키지로, import문 추가하여 사용 가능
+		Date today = Calendar.getInstance().getTime();
+		System.out.println(today);
+		
+	}
+
+}
