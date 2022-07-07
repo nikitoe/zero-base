1d2b3bdb68a55b09708f12a99e472a7e17efc016
Jiyong Sim <sjy19910222@gmail.com>
2022-07-07 12:12:34 +0900

    Part02 자료구조/알고리즘 04. 알고리즘-백트래킹

--- Part02. 자료구조 알고리즘/코딩예제/JAVA/src/Main.java
+++ Part02. 자료구조 알고리즘/코딩예제/JAVA/src/Main.java
@@ -1,93 +1,47 @@
-// 알고리즘 - 정렬_1
-
-import java.util.Arrays;
+// 알고리즘 - 백트래킹
 
 public class Main {
-    // 오름차순 기준 정렬 알고리즘
+    static int n = 4;
+    static int[] board = new int[n];
+    static int cnt;
 
-    // 버블 정렬
-    public static void bubbleSort(int[] arr) {
-        for (int i = 1; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - i; j++) {
-                if(arr[j] > arr[j + 1]){
-                    int tmp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = tmp;
-                }
+    public static int nQueen(int row) {
+        // 정상적으로 다 놓은 경우
+        if (row == n) {
+            cnt++;
+            for (int i = 0; i < n; i++) {
+                System.out.print(board[i] + " ");
             }
+            System.out.println();
+            return cnt;
         }
 
-        for (int i = arr.length - 1; i > 0 ; i--) {
-            for (int j = 0; j < i; j++){
-                if(arr[j] > arr[j + 1]){
-                    int tmp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = tmp;
-                }
-            }
-        }
-    }
-    
-    // 삽입 정렬
-    public static void insertionSort(int[] arr) {
-        for (int i = 1; i < arr.length; i++) {
-            for (int j = i; j > 0; j--) {
-                if(arr[j] < arr[j - 1]){
-                    int tmp = arr[j];
-                    arr[j] = arr[j - 1];
-                    arr[j - 1] = tmp;
-                } else {
-                    break;
-                }
-            }
-        }
+        // 각 행 각 열에 퀸을 놓으면서 가능성 검사
+        for (int i = 0; i < n; i++) {
+            // row 행에 i 열에 퀸 위치시킴 (1차원 배열로 열의 번호를 기록)
+            board[row] = i;
 
+            // 유망성 체크
+            if (isPromising(row)) {
+                nQueen(row + 1);
+            }
+        }
+        return cnt;
     }
 
-    // 선택 정렬
-    private static void selectionSort(int[] arr) {
-        // 최소 기준
-        for (int i = 0; i < arr.length -1; i++) {
-            int min = i;
-            for (int j = i +1; j < arr.length; j++) {
-                if(arr[j] < arr[min]){
-                    min = j;
-                }
+    public static boolean isPromising(int row) {
+        for (int i = 0; i < row; i++) {
+            // 같은 열에 놓은 경우 false
+            // 대각선에 있는 경우 false
+            if (board[row] == board[i] || row - i == Math.abs(board[row] - board[i])) {
+                return false;
             }
-            int tmp = arr[i];
-            arr[i] = arr[min];
-            arr[min] = tmp;
         }
-        
-        // 최대 기준
-        for (int i = arr.length - 1; i > 0 ; i--) {
-            int max = i;
-            for (int j = i - 1; j >=0 ; j--) {
-                if(arr[j] >arr[max]){
-                    max = j;
-                }
-
-            }
-            int tmp = arr[i];
-            arr[i] = arr[max];
-            arr[max] = tmp;
-        }
-
+        return true;
     }
 
     public static void main(String[] args) {
         // Test code
-        int[] arr = {3, 5, 2, 7, 1, 4};
-        bubbleSort(arr);
-        System.out.println("버블 정렬: " + Arrays.toString(arr));
-
-        arr = new int[]{3, 5, 2, 7, 1, 4};
-        insertionSort(arr);
-        System.out.println("삽입 정렬: " + Arrays.toString(arr));
-
-        arr = new int[]{3, 5, 2, 7, 1, 4};
-        selectionSort(arr);
-        System.out.println("선택 정렬: " + Arrays.toString(arr));
-
+        System.out.println("경우의 수: " + nQueen(0));  // 2
     }
 }
